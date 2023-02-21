f5cf7b657deb611a0518ce60a9097748df03dcad
mstgst <mostlygostly@gmail.com>
2023-02-21 10:47:45 -0500

    update

--- Kata/Java/greed.java
+++ Kata/Java/greed.java
@@ -62,6 +62,14 @@ public class Greed{
   }
 
   //Score tracker for the game 'greed'
-  //Ideally this would be compartmentalized into another class, for the purpose of a Kata it's all in one place
-  //Iterates through dice results to tally points based on sets of 3 equivalent dice roll or one and five die rolls
-  
+
+  public class Greed {
+
+    public static int greedy(int[] dice) {
+      int n[] = new int[7];
+      for (int d : dice) n[d]++;
+      return n[1]/3*1000 + n[1]%3*100 + n[2]/3*200 + n[3]/3*300 + n[4]/3*400 + n[5]/3*500 + n[5]%3*50 + n[6]/3*600;
+    }
+  }
+
+  //Optimized solution
